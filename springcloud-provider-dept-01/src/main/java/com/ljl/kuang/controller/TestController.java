a93828f9c4d9b9ee527491bfc1d096f6d8147c69
18501992045 <ljl13373794956@163.com>
2021-11-11 14:32:14 +0800

    对的

---
+++ springcloud-provider-dept-01/src/main/java/com/ljl/kuang/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ljl.kuang.controller;
+
+/**
+ * ClassName:TestController
+ * Package:com.ljl.kuang.controller
+ * Descriptions:
+ *
+ * @Data$:{DATE} 14:30
+ * @Author:ljl
+ **/
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("嘿哈");
+    }
+
+
+
+}
